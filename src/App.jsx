0bf54f3e154834fe8726e7934f861c817f0a8f0d
JoseEscalona18 <jose.al.es301@gmail.com>
2023-10-01 19:01:51 -0400

    Actualizacion1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
 import { useState } from 'react'
-import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+
+
+      <input
+          type="button"
+          value="Agregar"
+          className="custom-input bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      />
+
+      <input
+          type="button"
+          value="Editar"
+          className="custom-input bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      />
+
+      <input
+          type="button"
+          value="Limpiar"
+          className="custom-input bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      />
+
+
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ["./index.html",
   "./src/.{js,ts,jsx,tsx}",],
   theme: {
     extend: {},
+    fontFamily: {
+      'roboto': ['Roboto', 'sans-serif'],
+      'raleway': ['Raleway', 'sans-serif'],
+    }
   },
-  plugins: [],
+  plugins: [
+    require('@material-tailwind/react')
+  ],
 }
 
