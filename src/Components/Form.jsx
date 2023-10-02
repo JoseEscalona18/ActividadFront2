ab1ed56716a0f4ed991d4fc86e3fa2d2e4b662ba
Luismonsalver <126512492+Luismonsalver@users.noreply.github.com>
2023-10-02 18:17:53 -0400

    Cambio formulario

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -29,7 +29,6 @@ const Form = () => {
 
     // Guardar la lista actualizada en el localstorage
     localStorage.setItem('productos', JSON.stringify(productos));
-    
 
     // Limpiar los campos del formulario
     setNombre('');
@@ -41,49 +40,61 @@ const Form = () => {
 
   return (
 <form onSubmit={handleSubmit} className="container mx-auto p-4 bg-gray-100 shadow-md rounded-lg">
-  <div className="flex flex-col gap-4">
-    <div className="flex items-center gap-2">
+  <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-1">
       <label className="text-gray-700 font-medium" htmlFor="nombre">
         Nombre:
       </label>
       <input
-        className="border border-gray-300 rounded px-2 py-1 focus:border-blue-500 focus:outline-none"
+        className="w-full max-w-xs border border-gray-300 rounded px-2 py-1 focus:border-blue-500 focus:outline-none"
         type="text"
         id="nombre"
         value={nombre}
         onChange={(e) => setNombre(e.target.value)}
       />
     </div>
-    <div className="flex items-center gap-2">
+    <div className="flex flex-col gap-2">
+      <label className="text-gray-700 font-medium" htmlFor="numeroSerie">
+        Número de Serie:
+      </label>
+      <input
+        className="w-full max-w-xs border border-gray-300 rounded px-2 py-1 focus:border-blue-500 focus:outline-none"
+        type="text"
+        id="numeroSerie"
+        value={numeroSerie}
+        onChange={(e) => setNumeroSerie(e.target.value)}
+      />
+    </div>
+    <div className="flex flex-col gap-2">
       <label className="text-gray-700 font-medium" htmlFor="color">
         Color:
       </label>
       <input
-        className="border border-gray-300 rounded px-2 py-1 focus:border-blue-500 focus:outline-none"
+        className="w-full max-w-xs border border-gray-300 rounded px-2 py-1 focus:border-blue-500 focus:outline-none"
         type="text"
         id="color"
         value={color}
         onChange={(e) => setColor(e.target.value)}
       />
     </div>
-    <div className="flex items-center gap-2">
+    <div className="flex flex-col gap-2">
       <label className="text-gray-700 font-medium" htmlFor="descripcion">
         Descripción:
       </label>
       <input
-        className="border border-gray-300 rounded px-2 py-1 focus:border-blue-500 focus:outline-none"
+        className="w-full max-w-xs border border-gray-300 rounded px-2 py-1 focus:border-blue-500 focus:outline-none"
         type="text"
         id="descripcion"
         value={descripcion}
         onChange={(e) => setDescripcion(e.target.value)}
       />
     </div>
-    <div className="flex items-center gap-2">
+    <div className="flex flex-col gap-2">
       <label className="text-gray-700 font-medium" htmlFor="precio">
         Precio:
       </label>
       <input
-        className="border border-gray-300 rounded px-2 py-1 focus:border-blue-500 focus:outline-none"
+        className="w-full max-w-xs border border-gray-300 rounded px-2 py-1 focus:border-blue-500 focus:outline-none"
         type="text"
         id="precio"
         value={precio}
@@ -92,7 +103,7 @@ const Form = () => {
     </div>
     
     <button
-      class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+      class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mt-4 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
       type="submit"
     >
       Guardar
